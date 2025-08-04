54e0751dca62e352993a3247dc1845de9bfad4cc
Yudongok <dongok210@gmail.com>
2025-08-04 19:18:36 +0900

    docs : Graph Linked List

---
+++ C++/코딩테스트 합격자되기/그래프.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+struct nodeInfo {
+    int V;
+    int W;
+};
+
+// 인접리스트 선언
+vector<vector<nodeInfo>> adjList;
+
+// 인접리스트를 만들고 예제 데이터로 채우는 함수
+void addEdge(int u, int v, int w){
+    adjList[u].push_back({v, w});
+}
+
+void printAdjList(){
+    for (int i = 0; i < adjList.size(); i++){
+        cout << "Node " << i << ":";
+        for (const auto &node : adjList[i]){
+            cout << " -> (V: " << node.V << ", W: " << node.W << ")" ;
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int N=5;
+    adjList.resize(N);
+    addEdge(1, 2, 3);
+    addEdge(2, 1, 6);
+    addEdge(2, 3, 5);
+    addEdge(3, 2, 1);
+    addEdge(3, 4, 13);
+    addEdge(4, 4, 9);
+    addEdge(4, 1, 42);
+
+    printAdjList();
+
+    return 0;
+}
